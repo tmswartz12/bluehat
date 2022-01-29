1fed2f5ed623151355f2b59f6affe4aa0177e317
Tyler Swartz <tmswartz12@gmail.com>
2022-01-29 17:16:03 -0500

    updated db

--- server/config/serverConfig.js
+++ server/config/serverConfig.js
@@ -1,9 +1,9 @@
 let mongoURL;
 
-require('dotenv').config();
+require("dotenv").config();
 
-if (process.env.NODE_ENV === 'development') {
-  mongoURL = 'mongodb://localhost:27017/tyler-react-typescript-boilerplate';
+if (process.env.NODE_ENV === "development") {
+  mongoURL = "mongodb://localhost:27017/bluehat-db";
 } else {
   mongoURL = process.env.MONGO_URL;
 }
